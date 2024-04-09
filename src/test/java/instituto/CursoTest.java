d1ffe1fd49c635407b3aee23539fc0b65586e46b
Diego <dierume09@gmail.com>
2024-04-09 20:37:33 +0200

    implementacion test <testAniadirAlumno>

---
+++ src/test/java/instituto/CursoTest.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit3TestClass.java to edit this template
+ */
+package instituto;
+
+import static junit.framework.Assert.assertTrue;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Diego
+ */
+public class CursoTest{
+
+    private Curso curso;
+    private Persona alumno1;
+    private Persona alumno2;
+
+    protected void setUp() {
+        curso = new Curso("1DAM");
+        alumno1 = new Persona(152673899, "Diego", 'M', 21, 11, 2005);
+        alumno2 = new Persona(871526722, "Mario", 'M', 23, 9, 2005);
+    }
+
+    /**
+     * Test of aniadirAlumno method, of class Curso.
+     */
+    public void testAniadirAlumno() {
+        curso.aniadirAlumno(alumno1);
+        curso.aniadirAlumno(alumno2);
+
+        assertTrue(curso.toString().contains(alumno1.toString()));
+        assertTrue(curso.toString().contains(alumno2.toString()));
+    }
+    
+}
